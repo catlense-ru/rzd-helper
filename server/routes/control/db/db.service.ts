61779da566b41ef837146ecadd8728ffce31d0f8
catlense <catlense@voronin.xyz>
2022-03-10 02:54:21 +0700

    attemp export db

--- server/routes/control/db/db.service.ts
+++ server/routes/control/db/db.service.ts
@@ -8,31 +8,109 @@ import System from '../../../old_db/systems.json'
 import mongoose from "mongoose"
 import Export from "../../../models/Export"
 
+import fs from 'fs'
+import path from 'path'
+import nodemailer from 'nodemailer'
+const json2csv = require('json2csv').parse
+// @ts-ignore
+import convertCsvToXlsx from '@aternus/csv-to-xlsx'
+
 export default class DB {
 
   async dataExport() {
-    mongoose.connection.db.listCollections({name: 'exports'}).next(() => {
-      mongoose.connection.db.dropCollection('exports', console.log)
-    })
-    const decisions = await Decision.find({})
+    // mongoose.connection.db.listCollections({ name: 'exports' }).next(() => {
+    //   mongoose.connection.db.dropCollection('exports', console.log)
+    // })
+    // const decisions = await Decision.find({})
 
-    decisions.forEach(async e => {
-      const comment:any = await Comment.findOne({uid: e.comment_id})
-      if(!comment) return;
-      const data = new Export({
-        comment: comment.comment,
-        decision: e.decision,
-        by_comment: comment.by_name,
-        by_decision: e.by_name
-      })
-      await data.save()
+    // decisions.forEach(async e => {
+    //   const comment: any = await Comment.findOne({ uid: e.comment_id })
+    //   if (!comment) return;
+    //   const data = new Export({
+    //     comment: comment.comment,
+    //     decision: e.decision,
+    //     by_comment: comment.by_name,
+    //     by_decision: e.by_name
+    //   })
+    //   await data.save()
+    // })
+
+    const result = await Export.find({})
+
+    const fields = [
+      {
+        label: 'comment',
+        value: 'Замечание'
+      },
+      {
+        label: 'decision',
+        value: 'Решение'
+      },
+      {
+        label: 'by_comment',
+        value: 'Замечание добавил'
+      },
+      {
+        label: 'by_decision',
+        value: 'Решение добавил'
+      }
+    ]
+
+    let csv = json2csv(result, { fields }, { withBOM: true })
+
+    console.log(csv)
+    return
+
+    const date = new Date().toLocaleDateString().replace('.', '-').replace('.', '-') + '-' + new Date().toLocaleTimeString().replace(':', '-').replace(':', '-')
+    const filePath = path.join(__dirname, 'exports', `export-${date}.csv`)
+
+    fs.writeFile(filePath, csv, (err) => {
+      if (err) {
+        return console.log(err)
+      } else {
+        setTimeout(function () {
+          fs.unlinkSync(filePath); // delete this file after 30 seconds
+        }, 30000)
+        let source = filePath
+        let destination = path.join(__dirname, 'exports', `export-${date}.xlsx`)
+        convertCsvToXlsx(source, destination)
+      }
     })
+
+    let smtpTransport = nodemailer.createTransport({
+      host: "smtp.yandex.ru",
+      port: 465,
+      auth: {
+        user: "catlense@voronin.xyz",
+        pass: "!ac@Xs3&7s"
+      }
+    })
+
+    let mail = {
+      from: 'catlense@voronin.xyz',
+      to: 'max@voronin.xyz',
+      subject: `Экспорт базы данных на ${new Date().toLocaleDateString()}`,
+      text: 'База данных в виде Excel таблицы',
+      attachments: [
+        {
+          file: `export-${new Date().toLocaleDateString()}.xlsx`,
+          path: path.join(__dirname, 'exports', `export-${date}.xlsx`)
+        }
+      ]
+    }
+
+    // smtpTransport.sendMail(mail, (err, res) => {
+    //   if(err) return console.log(err)
+    //   console.log(`Message send ${res.response}`)
+    //   smtpTransport.close()
+    // })
+
 
     return "ok"
 
   }
 
-  async transfer() : Promise<Object> {
+  async transfer(): Promise<Object> {
 
     // Comments.forEach(async c => {
     //   const comments = new Comment({
@@ -43,7 +121,7 @@ export default class DB {
     //     by_name: c.by_name,
     //     train: c.train
     //   })
-  
+
     //   await comments.save()
     // })
 
@@ -55,7 +133,7 @@ export default class DB {
     //     by: 2,
     //     by_name: c.by_name,
     //   })
-  
+
     //   await comments.save()
     // })
 
@@ -66,12 +144,12 @@ export default class DB {
     //     by: 2,
     //     by_name: c.by_name,
     //   })
-  
+
     //   await comments.save()
     // })
 
 
-    return {sys: System.length, com: Comments.length, dec: Decisions.length}
+    return { sys: System.length, com: Comments.length, dec: Decisions.length }
   }
 
 }
