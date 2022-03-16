ea1bb4f4bc6422cfc5d26de6c6aacea946dabe61
Maxim <catlense@voronin.xyz>
2022-03-17 02:57:15 +0700

    test

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line
 export default {
-  // api: 'http://localhost:1001',
+  api: 'http://localhost:1001',
   // api: 'http://192.168.0.110:1001'
-  api: 'https://helper.voronin.xyz'
+  // api: 'https://helper.voronin.xyz'
 }
