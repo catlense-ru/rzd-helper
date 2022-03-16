9a7aa99b6d84f5645d4a3335011707d1cb7d9e02
Maxim <catlense@voronin.xyz>
2022-03-16 23:05:39 +0700

    change api

--- server/index.ts
+++ server/index.ts
@@ -34,7 +34,7 @@ const imageUpload = multer({
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use('/images', express.static(path.join(__dirname, 'images')))
+app.use('/images', express.static(path.join(__dirname, '../', 'images')))
 app.use((_, __, next) => {
   next()
 })
