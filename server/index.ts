161c7d58a70ee7c3f90d12171e6a047428378415
Maxim <catlense@voronin.xyz>
2022-03-16 23:08:44 +0700

    change

--- server/index.ts
+++ server/index.ts
@@ -34,7 +34,7 @@ const imageUpload = multer({
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use('/images', express.static(path.join(__dirname, '../', 'images')))
+// app.use('/images', express.static(path.join(__dirname, '../', 'images')))
 app.use((_, __, next) => {
   next()
 })
@@ -57,7 +57,7 @@ app.post('/upload', imageUpload.single('photo'), (req: Request, res: Response) =
 
 app.get('/images/:filename', (req: Request, res: Response) => {
   const { filename } = req.params
-  return res.sendFile(path.join(__dirname, 'images', filename))
+  return res.sendFile(path.join(__dirname, '../', 'images', filename))
 })
 
 app.listen(process.env.PORT, () => console.log(`Server has been started ${process.env.PORT}`))
